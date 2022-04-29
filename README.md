9db622804465c9a3ed318acd6afb86662adc0b2b
Matias Schmidt <matias.schmidt@outlook.es>
2022-04-29 17:33:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## React Hooks Timer App
+
+Haremos una App sencilla, utilizando los nuevos features de React, __Hooks__, que nos permiten el uso de manejos de estados y llevar a cabo efectos secundarios en nuestros componentes funcionales.
+
+Nuestra App sera un Contador y Cuenta regresiva. Podremos cambiar entre ambos y escribir una cantidad de segundos para la cuentra regresiva.
+
+Tendremos un solo componente en donde manejaremos los estados y nuestra UI. 
+
+
+Ejecutamos `npm install` en la consola para instalar las dependencias, una vez instaladas inicializamos el proyecto con `npm start`. Se iniciara en `http://localhost:3000/`.
+
---
+++ src/Timer.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Timer.css';
+
+
+const Timer = () => {
+  const [segundos, setSegundos] = useState(0);
+  const [activo, setActivo] = useState(false);
+  const [tipo, setTipo] = useState('Contador');
+
+  function toggle() {
+    setActivo(!activo);
+  }
+
+  function reset() {
+    setSegundos(0);
+    setActivo(false);
+  }
+
+  function cambioTipo() {
+    if(tipo === 'Contador') setTipo('Cuenta Regresiva')
+    if(tipo === 'Cuenta Regresiva') setTipo('Contador')
+  }
+
+  useEffect(() => {
+    let intervalo = null;
+    if (activo && tipo === 'Contador') {
+      intervalo = setInterval(() => {
+        setSegundos(segundos => segundos + 1);
+      }, 1000);
+    }
+    if (activo && tipo === 'Cuenta Regresiva') {
+      intervalo = setInterval(() => {
+        setSegundos(segundos => segundos - 1);
+      }, 1000);
+    }
+    if (!activo && segundos !== 0 && tipo === 'Contador') {
+      clearInterval(intervalo);
+    }
+    if (segundos <= 0 && tipo === 'Cuenta Regresiva') {
+      reset();
+      clearInterval(intervalo);
+    }
+
+
+    return () => clearInterval(intervalo);
+  }, [activo, segundos, tipo]);
+  const myRef = useRef(null);
+
+  function agregaSegundos() {
+    // `current` apunta al elemento de entrada de texto montado
+    let ref = myRef.current.value
+    setSegundos(Number(ref));
+}
+
+
+
+  return (
+    <div className="app">
+      <div className="time">
+        {segundos}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${activo ? 'active' : 'inactive'}`} onClick={toggle}>
+          {activo ? 'Pausa' : 'Inicio'}
+        </button>
+        <button className="button button-primary" onClick={reset}>
+          Reset
+        </button>
+        <button className="button button-secondary" onClick={cambioTipo}>
+          {tipo}
+        </button>
+        {tipo === 'Cuenta Regresiva' && <input className='input' type="number" ref={myRef} onChange={agregaSegundos} placeholder="Ingresa Segundos" autoComplete="off"/>}
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
